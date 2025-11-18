71a88cf5d86f1c9b302a3f497b670ff5fe72e796
peterpipes776 <92380593+peterpipes776@users.noreply.github.com>
2025-11-18 10:13:05 +0000

    Add function

---
+++ firstrepo.js
@@ -0,0 +1,7 @@
+//This is a ts file
+//function to add two numbers
+function Add(a, b) {
+    return a + b;
+}
+var num = Add(1, 2);
+console.log(num);
---
+++ firstrepo.ts
@@ -0,0 +1,10 @@
+//This is a ts file
+
+
+//function to add two numbers
+function Add(a:  number, b: number) : number {
+    return a + b;
+}
+
+const num = Add(1, 2);
+console.log(num);
