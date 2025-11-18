a22cf081922520d26db95b124c9246004977b05c
peterpipes776 <92380593+peterpipes776@users.noreply.github.com>
2025-11-18 10:43:07 +0000

    add Divide Multiply

--- firstrepo.js
+++ firstrepo.js
@@ -9,8 +9,23 @@ function Subtract(a, b) {
     return a - b;
 }
 
+//function to multiply two numbers
+function Multiply(a, b) {
+    return a * b;
+}
+
+//function to divide two numbers
+function Divide(a, b) {
+    return a / b;
+}
 var num = Add(1, 2);
 console.log(num);
 
 var num2 = Subtract(1, 2);
 console.log(num2);
+
+var num3 = Multiply(1, 2);
+console.log(num3);
+
+var num4 = Divide(1, 2);
+console.log(num4);
