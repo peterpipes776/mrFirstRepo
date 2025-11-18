4d1c22aa8ae09427bc23589fa1aa1d17b17112fd
peterpipes776 <92380593+peterpipes776@users.noreply.github.com>
2025-11-18 10:33:46 +0000

    reset master

--- firstrepo.js
+++ firstrepo.js
@@ -3,5 +3,14 @@
 function Add(a, b) {
     return a + b;
 }
+
+//function to subtract two numbers
+function Subtract(a, b) {
+    return a - b;
+}
+
 var num = Add(1, 2);
 console.log(num);
+
+var num2 = Subtract(1, 2);
+console.log(num2);
